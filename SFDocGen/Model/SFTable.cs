d3ee5fbcc6fcc81d9b3dd48b57263727e39fc30f
The17thDoctor <ewhorville@gmail.com>
2026-07-05 11:04:28 +0200

    Modified struct fields to be able to define default values

--- SFDocGen/Model/SFTable.cs
+++ SFDocGen/Model/SFTable.cs
@@ -16,13 +16,13 @@ public record SFTable : SFDocElement, IHasRealm
         if (Description != null) sb.AppendLine("---" + Description.Replace("\n", "<br>\n---"));
         sb.AppendLine($"---@class {Name}");
 
+        sb.AppendLine($"{Name} = {{}}");
+
         if (Fields.Count > 0)
         {
-            sb.AppendJoin("\n", Fields.Select(f => f.ToLuaDoc()));
             sb.AppendLine();
+            sb.AppendJoin("\n", Fields.Select(f => f.ToLuaDoc()));
         }
-
-        sb.Append($"{Name} = {{}}");
 
         return sb.ToString();
     }
@@ -31,6 +31,7 @@ public record SFTable : SFDocElement, IHasRealm
 public record SFTableField : SFDocValue, IChildObject<SFTable>
 {
     public string Type { get; set; } = string.Empty;
+    public string DefaultValue { get; set; } = "nil";
 
     [JsonIgnore]
     public SFTable Parent { get; init; } = default!;
@@ -45,6 +46,9 @@ public record SFTableField : SFDocValue, IChildObject<SFTable>
             sb.Append(' ');
             sb.Append(Description.Replace("\n", "\n---"));
         }
+
+        sb.AppendLine();
+        sb.AppendLine($"{Parent.Name}.{Name} = {DefaultValue}");
 
         return sb.ToString();
     }
